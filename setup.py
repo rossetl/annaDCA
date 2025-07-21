fda9e997e7f36bd91b85d9ad3bbc16730c26371f
rossetl <rosset.lorenzo@gmail.com>
2025-07-21 10:17:54 +0200

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='annadca',
-    version='0.1.0',
+    version='0.2.0',
     author='Lorenzo Rosset, Aurélien Decelle, Beatriz Seoane, Francesco Zamponi, Martin Weigt',
     maintainer='Lorenzo Rosset',
     author_email='rosset.lorenzo@gmail.com',
